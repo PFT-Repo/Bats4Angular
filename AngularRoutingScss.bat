434e82c2d64487214a564b422f02db3afda88498
TurpialCode <84598876+PFT-Repo@users.noreply.github.com>
2023-01-19 12:10:22 +0100

    Add files via upload

---
+++ AngularRoutingScss.bat
@@ -0,0 +1,9 @@
+@echo off
+start cmd.exe /k "cd %UserProfile%\Desktop> && MKDIR test"
+start cmd.exe /k "MKDIR test"
+start cmd.exe /k "cd %UserProfile%\Desktop> && MKDIR test"
+pause
+echo Cuando finalice la instalación, cierra y dale ENTER
+start cmd.exe /k "cd %UserProfile%\Desktop/test && ng new prueba --skip-install --routing --style scss && cd C:\Users\formacio\Desktop/test/prueba && ng g m components/components && ng g service service/service"
+pause
+start cmd.exe /k "cd %UserProfile%\Desktop/test/prueba  && code . && npm i"
